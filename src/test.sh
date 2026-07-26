874b22c517e6ffa0efd95c9ac370aadab433e528
Abdullah shehata <abdullahshehata90@gmail.com>
2026-07-26 05:37:42 +0300

    Creating A New NodeJs App

---
+++ src/test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+
+# src /test.sh 
+
+EXPECTED = "Hello , Test !" 
+
+OUTPUT = $ (node -e "console.log(require('./src/app')('Test'))")
+
+
+if  [$OUTPUT == $EXPECTED] ; then 
+
+echo " Test Passed !" 
+
+exit 0 
+
+else 
+
+echo " Test failed !  EXPECTED $EXPECTED but got  $OUTPUT"
+
+exit 1 
+fi 
